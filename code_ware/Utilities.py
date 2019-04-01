d89448f57311236755528f3289b16371695c6418
chrislhchoo <chris.lhchoo@gmail.com>
2019-04-01 21:13:14 +0800

    Utilities

--- code_ware/Utilities.py
+++ code_ware/Utilities.py
@@ -18,12 +18,13 @@ class converter:
         ret=self.str2date(str(ret).split(' ')[0])
         return ret
 if __name__=="__main__":
-    a=converter()
-    print(a.tsp2dt(1545730074))
-    count=100000
-    t0 = 1000*timeit.timeit('start_date = datetime.datetime.strptime(a, "%Y-%m-%d")',
-                            "import datetime\na='2019-9-9'", number=count)
-    t0=round(t0,2)
-    print(f'{t0}')
+    pass
+    # a=converter()
+    # print(a.tsp2dt(1545730074))
+    # count=100000
+    # t0 = 1000*timeit.timeit('start_date = datetime.datetime.strptime(a, "%Y-%m-%d")',
+    #                         "import datetime\na='2019-9-9'", number=count)
+    # t0=round(t0,2)
+    # print(f'{t0}')
 
     #a+=1
