1b1b2a7a55996208b9537a489b89a2c10ffc4a11
chrislhchoo <chris.lhchoo@gmail.com>
2019-04-08 23:33:32 +0800

    Utilities: 1 STR=>DATE supported format  '20190909'  '20190101010101'  '201901010101'  '2019-01-01 00:02:02'  '2019-01-01 00:02'  '2019/01/01 00:02:02'  '2019/01/01 00:02'  'Mar 2019 1'  'Mar 1 2019'

--- code_ware/cantStop.py
+++ code_ware/cantStop.py
@@ -2,25 +2,26 @@ import time
 from multiprocessing import Queue,Process,cpu_count
 from queue import Empty
 
-def add(q, index):
+def add(q, index,Q):
     r = 0
     while not q.empty():
         try:
             args = q.get(block=False)
-            r = args[0] * args[1]
+            Q.put(args[0])
         except Empty:
-            print(f'{index}_Break')
+            #print(f'{index}_Break')
             pass
     print(f'{index}_QueueIsEmpty')
 
 if __name__ == '__main__':
     past ,cpus= time.time(),cpu_count()
+    Q= Queue()
     for j in range(1):
-        qq, q, p = Queue(), Queue(), []
+        q, p = Queue(), []
         for i in range(1000):
             q.put((i, i + 1))
         for i in range(cpus):
-            p.append(Process(target=add, args=(q, i)))
+            p.append(Process(target=add, args=(q, i,Q)))
         for i in p:
             i.start()
         for i in p:
@@ -28,3 +29,8 @@ if __name__ == '__main__':
         print("JoinFinished")
         ts = round((time.time() - past) * 1000, 2)
     print('ProgramStoped')
+    rs=[]
+    while not Q.empty():
+        rs.append(Q.get())
+    rs.sort()
+    print(len(rs))
