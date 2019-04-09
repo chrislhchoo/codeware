b44df6dc660eeabe03288d0a499245c50b479f44
chrislhchoo <49101200+chrislhchoo@users.noreply.github.com>
2019-04-09 10:29:29 +0800

    Update cantStop.py

--- code_ware/cantStop.py
+++ code_ware/cantStop.py
@@ -1,26 +1,34 @@
-import time
-from multiprocessing import Queue,Process,cpu_count
-from queue import Empty
+# -*- coding: utf-8 -*-
+"""
+*
+* copyright (c) 2016, 2017 State Street Bank & Trust Co.
+% Description:	Utils for Python3
+"""
+import re, datetime, time
+from multiprocessing import Pool, cpu_count, Queue, Process
+from _queue import Empty
+import multiprocessing
 
 def add(q, index,Q):
     r = 0
-    while not q.empty():
-        try:
-            args = q.get(block=False)
-            #Q.put(args[0])
-        except Empty:
-            print(f'{index}_Empty')
-            pass
-        except:
-            print('else')
+    try:
+        args = q.get(block=False)
+        #Q.put(args[0])
+    except Empty:
+        #print(f'{index}_Empty')
+        pass
+    except:
+        print('else')
     print(f'{index}_QueueIsEmpty')
 
+
 if __name__ == '__main__':
-    past ,cpus= time.time(),cpu_count()*4
+    multipy=1
+    past ,cpus= time.time(),cpu_count()*multipy
     Q= Queue()
     for j in range(1):
         q, p = Queue(), []
-        for i in range(2000000):
+        for i in range(20000):
             q.put((i, i + 1))
         diff=round(time.time()-past,2)*1000
         print(f'PutCost{diff}MS')
@@ -36,15 +44,6 @@ if __name__ == '__main__':
         print(f'LoopCost{diff}MS')
         past=time.time()
         ts = round((time.time() - past) * 1000, 2)
-    print('ProgramStoped')
-    rs=[]
-    # while not Q.empty():
-    #     rs.append(Q.get())
-    diff = round(time.time() - past, 2) * 1000
-    print(f'AppendCost{diff}MS')
-    past = time.time()
-    rs.sort()
-    diff = round(time.time() - past, 2) * 1000
-    print(f'SortCost{diff}MS')
-    past = time.time()
-    print(len(rs))
+    print('ProgramRunToTheEnd')
+    #diff = round(time.time() - past, 2) * 1000
+
