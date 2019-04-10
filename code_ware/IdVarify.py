6833052db654309cf59c220fc8688035273dde3c
chrislhchoo <chris.lhchoo@gmail.com>
2019-04-10 22:46:57 +0800

    Utilities: 1 STR=>DATE supported format  '20190909'  '20190101010101'  '201901010101'  '2019-01-01 00:02:02'  '2019-01-01 00:02'  '2019/01/01 00:02:02'  '2019/01/01 00:02'  'Mar 2019 1'  'Mar 1 2019'

---
+++ code_ware/IdVarify.py
@@ -0,0 +1,77 @@
+from datetime import datetime as dtm
+
+w = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1]
+div = 11
+mod = [1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+province = {"11": "北京", "12": "天津", "13": "河北", "14": "山西", "15": "内蒙古", "21": "辽宁", "22": "吉林", "23": "黑龙江",
+            "31": "上海", "32": "江苏", "33": "浙江", "34": "安徽", "35": "福建", "36": "江西", "37": "山东", "41": "河南", "42": "湖北",
+            "43": "湖南", "44": "广东", "45": "广西", "46": "海南", "50": "重庆", "51": "四川", "52": "贵州", "53": "云南", "54": "西藏",
+            "61": "陕西", "62": "甘肃", "63": "青海", "64": "宁夏", "65": "新疆", "71": "台湾", "81": "香港", "82": "澳门", "91": "国外"}
+month = {'01': 'JAN', '02': 'FEB', '03': 'MAR', '04': 'APR', '05': 'MAY', '06': 'JUN', '07': 'JUL', '08': 'AUG',
+         '09': 'SEP', '10': 'OCT', '11': 'NOV', '12': 'DEC'}
+
+
+def varify(card):
+    if len(card) != 18:
+        return '身份证长度错误'
+    try:
+        # 除最后一位以外ID
+        id = card[:-1]
+        tail = card[-1]
+        tail = int(tail) if tail != 'X' else 10
+        pro = card[0:2]
+        gender = 'Female' if int(card[-2]) % 2 == 0 else 'Male'
+    except:
+        return '身份证输入错误'
+    try:
+        birth = card[6:14]
+        y = birth[0:4]
+        birthday = birth[4:8]
+        mo = month[birthday[0:2]]
+        birthday = f'{mo}-{birthday[2:4]}'
+        birth = dtm.strptime(birth, "%Y%m%d")
+        now = dtm.now()
+        age = (now - birth).days // 365
+    except ValueError:
+        return ('身份证8位日期错误')
+    except Exception as e:
+        print(e)
+    s = 0
+    for i in range(len(id)):
+        j = int(id[i])
+        add = j * w[i]
+        s += add
+    dived = s % div
+    dived = mod[dived]
+
+    ret = {
+        'Varify': dived == tail,
+        'Gender': gender,
+        'Birth': birth,
+        'Birthday': birthday,
+        'Porvince': province[pro],
+        'Age': age
+    }
+    return ret
+
+
+if __name__ == '__main__':
+    PP = [{'Name': '吕卓', 'ID': '450821198807025619'},
+          {'Name': '陈舒伟', 'ID': '452501198505017437'},
+          {'Name': '岑鹏飞', 'ID': '330381198710130916'},
+          {'Name': '叶飞阳', 'ID': '330327197412077149'},
+          {'Name': '张可忠', 'ID': '330327197304056092'},
+          {'Name': '陈照花', 'ID': '330327194202230420'},
+          {'Name': '林宣煊', 'ID': '330327196206290040'},
+          {'Name': '曾薇薇', 'ID': '330327198701290426'},
+          {'Name': '朱思阁', 'ID': '352231195910220011'},
+          {'Name': '朱为国', 'ID': '33032719401107041X'},
+          {'Name': '朱琰', 'ID': '33032720140616005X'},
+          {'Name': '杨忠将', 'ID': '330326197911194116'}]
+    for i in PP:
+        for k,v in i.items():
+            if 'Name' in k:
+                print(v)
+            if 'ID' in k:
+                print(varify(v))
+        print('##############')
--- code_ware/cantStop.py
+++ code_ware/cantStop.py
@@ -2,56 +2,56 @@
 """
 auth by:Chris
 """
-import re, datetime, time
+import re
+import time as ti
 from multiprocessing import Pool, cpu_count, Queue, Process
-from _queue import Empty
+from queue import Empty
 import traceback
 
-def add(q, index,Q):
+
+def add(leakqueue, index):
     r = 0
     while 1:
         try:
-            args = q.get(block=False)
+            args = leakqueue.get(block=False)
+            if args[0] == 'exit':
+                print(f'进程 {index} 获取退出指令，退出')
+                break
+            print(f'进程 {index} 正正处理{args}')
         except Empty:
             print(f'进程 {index} 获取输入队列为空')
         except:
             print('未知异常:')
             traceback.print_exc()
 
-        if args[0] == 'exit':
-            print(f'进程 {index} 获取退出指令，退出')
-            break
-        print(f'进程 {index} 正正处理{args}')
-
-        time.sleep(0.01)
-    # print(f'{index}_QueueIsEmpty')
+        ti.sleep(0.01)
+        # print(f'{index}_QueueIsEmpty')
 
 
 if __name__ == '__main__':
-    multipy=1
-    past ,cpus= time.time(),cpu_count()*multipy
-    Q= Queue()
+    multipy = 1
+    past, cpus = ti.time(), cpu_count() * multipy
+    Q = Queue()
     for j in range(1):
         q, p = Queue(), []
         # 数据项
         for i in range(20000):
-            q.put(('ops',i, i + 1))
+            q.put(('ops', i, i + 1))
         # 控制退出项
         for i in range(cpus):
-            q.put(('exit',i, i + 1))
-        diff=round(time.time()-past,2)*1000
+            q.put(('exit', i, i + 1))
+        diff = round(ti.time() - past, 2) * 1000
         print(f'PutCost{diff}MS')
-        past=time.time()
+        past = ti.time()
         for i in range(cpus):
-            p.append(Process(target=add, args=(q, i,Q)))
+            p.append(Process(target=add, args=(q, i)))
         for i in p:
             i.start()
         for i in p:
             i.join()
         print("JoinFinished")
-        diff=round(time.time()-past,2)*1000
+        diff = round(ti.time() - past, 2) * 1000
         print(f'LoopCost{diff}MS')
-        past=time.time()
-        ts = round((time.time() - past) * 1000, 2)
+        past = ti.time()
+        ts = round((ti.time() - past) * 1000, 2)
     print('ProgramRunToTheEnd')
-
