a07cc8ca275fc4eba71d206de677b5a7e87bc085
chrislhchoo <49101200+chrislhchoo@users.noreply.github.com>
2019-06-18 17:16:06 +0800

    Update timeout.py

--- code_ware/timeout.py
+++ code_ware/timeout.py
@@ -12,10 +12,37 @@ def task():
         print('hello world')
         time.sleep(1)
 
+
+def performance(func):
+    def wrapper(*args, **kwargs):
+        past = time.time()
+        func(*args, **kwargs)
+        timediff = round((time.time() - past) * 1000, 2)
+        print(timediff)
+
+    return wrapper
+
+
+@performance
+def add(adding):
+    r = 0
+    for i in range(adding):
+        r += i
+
+
+@performance
+def beep(adding):
+    r = 0
+    for i in range(adding):
+        r += i
+    for i in range(adding):
+        r += i
+
+
 if __name__ == '__main__':
     past = time.time()
     try:
         task()
     except func_timeout.exceptions.FunctionTimedOut:
         print(f'task func_timeout,costs {round((time.time()-past)*1000,2)} MS')
-
+    beep(10000000)
